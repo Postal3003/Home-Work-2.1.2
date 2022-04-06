000f6c0decf29f9d66d0fc23f4aee6f5dfa3d6ee
Dmitry <borodulkin_d@mail.ru>
2022-04-06 12:29:42 +0300

    The project is ready but changing the text on the button does not work

--- Home Work 2.1.2/ViewController.swift
+++ Home Work 2.1.2/ViewController.swift
@@ -20,13 +20,48 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.4
+    
     // методы переопределения, айби экшаны, публичные методы класса, приватные методы
     
-    override func viewDidLoad() {
+    override func viewDidLoad() { //настройка нашего интерфейса
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        startButton.layer.cornerRadius = 10 //скругление кнопки
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
     }
-
+    override func viewWillLayoutSubviews() { //скругление квадратов
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
+    }
+    
+    @IBAction func startButtonPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightIsOn
+            yellowLight.alpha = lightIsOff
+            currentLight = .red
+        }
+    }
+    
 
 }
 
